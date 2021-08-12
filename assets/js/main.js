a9563f5c9e8b9cbd4e3d446c655f4ec7040074aa
Russell Johnson <joh14233@umn.edu>
2021-08-12 16:24:46 -0700

    Making final changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,4 +141,26 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+// var sr = ScrollReveal({
+//     distance: '60px',
+//     duration: 2800,
+// })
+//
+// sr.reveal(`.home__data, .home__social-link, .home__info,
+//             .discover__container,
+//             .experience__data, .experience__overlay,
+//             .place__card,
+//             .sponsor__content,
+//             .footer__data, .footer__rights`, {
+//             origin: 'top',
+//             interval: 100,
+// })
+//
+// sr.reveal(`.about__data,
+// .video__description,
+// .subscribe__description`, {
+//     origin: 'left',
+// })
+
 
